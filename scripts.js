fe9c3969e29bfbbf011cf3155f4f357065b7eb99
“MarkGregory1 git config --global user.email “markgregory016@gmail.com <student@Cumberlandss-MacBook-Pro-10.local>
2022-02-28 23:02:25 -0500

    JSBasicsExercise scripts.js

---
+++ scripts.js
@@ -0,0 +1,22 @@
+console.log(52);
+// Numbers, Strings, Booleans, Null, Undefined
+// + _ * / % **
+console.log(5 % 2); console.log(11 % 5);
+console.log("I'M MARY POPPINS, Y'ALL");
+console.log("Hawk" + "eye");
+console.log("Mark Gregory".length);
+console.log("Avengers: Endgame"[16]);
+
+// BONUS
+console.log("\\");
+console.log(5 ** 4 - 9);
+console.log("I".length - "Am".length + "Iron".length - "Man".length);
+console.log("CAP"[1]);
+console.log("VISION"[0]);
+console.log("OKOYE"[4]);
+console.log("NICK FURY"[0]);
+console.log("GROOT"[0]);
+console.log("DOCTOR STRANGE"[13]);
+console.log("THOR"[3]);
+console.log("STAR-LORD"[0]);
+console.log((u) = (m=25) + (c=4));
